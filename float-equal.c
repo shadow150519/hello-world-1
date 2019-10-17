96291eec7d69dcce9288592fe495905655c84c70
shadow150519 <55205022+shadow150519@users.noreply.github.com>
2019-10-17 20:17:41 +0800

    Add files via upload

--- float-equal.c
+++ float-equal.c
@@ -1,12 +1,13 @@
 //实现代码判断2个float变量相等
 //误差精确到时小数点后5位
 #include <stdio.h>
+#define ESP 0.00001 
 int main()
 {
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (fabs(z - z1)<ESP) {
         z = (m + x) / y;
     }
     printf("Z is %f\n", z);
